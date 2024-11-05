558e3c07a93ac98c5ded2ebfa7d6d7ea6c100024
Juan Duarte <juan.devcontact@gmail.com>
2024-11-05 00:57:39 -0500

    Modificando helper para contar caracteres especiales en las palabras

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -13,10 +13,10 @@ class StringHelper
         }
 
         // Tomamos el primer carácter de la cadena y lo convertimos en mayúscula
-        $letra = strtoupper($string[0]);
+        $letra = mb_strtoupper(mb_substr($string, 0, 1));
 
-        // Solo procesamos letras (ignoramos espacios y caracteres especiales)
-        if (ctype_alpha($letra)) {
+        // Solo procesamos letras, incluyendo letras con tildes y la ñ
+        if (preg_match('/^[A-ZÁÉÍÓÚÑ]$/u', $letra)) {
             // Incrementamos el contador de esa letra
             if (isset($resultado[$letra])) {
                 $resultado[$letra]++;
@@ -26,6 +26,6 @@ class StringHelper
         }
 
         // Llamada recursiva con el resto de la cadena
-        return self::contarLetras(substr($string, 1), $resultado);
+        return self::contarLetras(mb_substr($string, 1), $resultado);
     }
 }
--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('contribuyentes.index', absolute: false));
+        return redirect()->intended(route('usuarios.index', absolute: false));
     }
 
     /**
